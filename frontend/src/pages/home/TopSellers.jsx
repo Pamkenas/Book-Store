113719b3c9734ffca3625382b092f1fda908bafe
Pamkenas <kimbiras@gmail.com>
2024-10-25 01:03:32 +0300

    initial commit

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const TopSellers = () => {
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
     fetch("books.json")
-  }, [])
+      .then(res => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
-  return (
-      <div>TopSellers</div>
-  )
-}
+  return <div>TopSellers</div>;
+};
 
-export default TopSellers
+export default TopSellers;
