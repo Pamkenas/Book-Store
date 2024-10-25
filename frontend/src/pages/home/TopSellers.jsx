f5d19aac8ba43bf5d45573c51ab56b8ea7bdf3df
Pamkenas <kimbiras@gmail.com>
2024-10-25 14:00:34 +0300

    initial commit

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from "react";
 
+const categories = [
+  "Choose a ganre",
+  "Business",
+  "Fiction",
+  "Horror",
+  "Adventure",
+];
+
 const TopSellers = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     fetch("books.json")
-      .then(res => res.json())
-      .then((data) => setBooks(data))
-  } , [])
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
 
   return (
     <div className="py-10">
       <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
       {/* category filtering */}
+      <div className="mb-8 flex items-center">
+        <select
+          name="category"
+          id="category"
+          className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
+        >
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopSellers;
