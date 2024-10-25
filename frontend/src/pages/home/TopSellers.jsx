f4100a7b55642d908c31bba1b2f9b7eb8394f67e
Pamkenas <kimbiras@gmail.com>
2024-10-26 00:08:25 +0300

    initial commit

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,56 +1,9 @@
-import React, { useEffect, useState } from "react";
-
-const categories = [
-  "Choose a ganre",
-  "Business",
-  "Fiction",
-  "Horror",
-  "Adventure",
-];
+import React from 'react'
 
 const TopSellers = () => {
-  const [books, setBooks] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("Choose genre");
-
-  useEffect(() => {
-    fetch("books.json")
-      .then((res) => res.json())
-      .then((data) => setBooks(data));
-  }, []);
-
-  const filteredBooks =
-    selectedCategory === "Choose genre"
-      ? books
-      : books.filter(
-          (books) => books.category === selectedCategory.toLowerCase()
-        );
-
-  console.log(filteredBooks);
-
   return (
-    <div className="py-10">
-      <h2 className="text-3xl font-semibold mb-6">Top Sellers</h2>
-      {/* category filtering */}
-      <div className="mb-8 flex items-center">
-        <select
-          onChange={(e) => setSelectedCategory(e.target.value)}
-          name="category"
-          id="category"
-          className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
-        >
-          {categories.map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div>TopSellers</div>
+  )
+}
 
-      {filteredBooks.map((book, index) => (
-        <div>{book.title}</div>
-      ))}
-    </div>
-  );
-};
-
-export default TopSellers;
+export default TopSellers
