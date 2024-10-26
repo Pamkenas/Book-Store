e745344bcc59aa6af73933a26a8478128dbad1d4
Pamkenas <kimbiras@gmail.com>
2024-10-26 23:39:56 +0300

    initial commit

--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
 
 const categories = [
   "Choose a genre",
@@ -8,7 +9,7 @@ const categories = [
   "Adventure",
 ];
 
-const TopSellers = () => {
+const TopSellers = ({ book }) => {
   const [books, setBooks] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
 
@@ -18,18 +19,26 @@ const TopSellers = () => {
       .then((data) => setBooks(data));
   }, []);
 
-  const filteredBooks = selectedCategory === "Choose a genre" ? books:books.filter(book => book.category === selectedCategory.toLowerCase())
+  const filteredBooks =
+    selectedCategory === "Choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
 
-  console.log(filteredBooks)
+  console.log(filteredBooks);
 
   return (
     <div className="py-10">
       <h2 className="text-3xl font-semibold mb-6">New Releases This Week</h2>
       {/* Category Filter */}
       <div className="mb-8 flex items-center">
-        <select 
+        <select
           onChange={(e) => setSelectedCategory(e.target.value)}
-        name="category" id="category" className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none">
+          name="category"
+          id="category"
+          className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none"
+        >
           {categories.map((category, index) => (
             <option key={index} value={category}>
               {category}
@@ -38,11 +47,9 @@ const TopSellers = () => {
         </select>
       </div>
 
-      {
-        filteredBooks.map((book, index) => (
-          <div>{book.title}</div>
-        ))
-      }
+      {filteredBooks.map((book, index) => (
+        <BookCard key={index} book={book} />
+      ))}
     </div>
   );
 };
