7eec496644cb8c25d89309b11694689bf87bda08
Pamkenas <kimbiras@gmail.com>
2024-10-27 00:26:53 +0300

    initial commit

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import TopSellers from "./TopSellers";
+import Recomened from "./Recommened"
 
 const Home = () => {
  return (
   <>
     <Banner/>
     <TopSellers/>
+    <Recomened/>
   </>
  )
 };
